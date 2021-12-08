157101d96fd4155a37f4fa29ee943e00e8f5180a
Erensurul <95777770+Erensurul@users.noreply.github.com>
2021-12-09 00:27:52 +0300

    Add files via upload

---
+++ pythonyemekuyg.py
@@ -0,0 +1,12 @@
+
+
+
+
+yemekayıt = open("yemektarifleri.txt", "w") 
+yemekayıt.write(input("Yemek Adı Giriniz = " )) 
+yemekayıt.write(input("Yemek Tarifi Giriniz = " ))
+yemekayıt.close()
+
+dosya = open("yemektarifleri.txt","r")
+oku = dosya.read()
+print("Yemek Adı ve Tarifiniz = " ,oku)
